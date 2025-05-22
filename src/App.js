89bceb161272bdf74b9d73cc63a33602e2805eed
Mahen Mahindaratne <mmahindaratne88@gmail.com>
2025-05-22 20:11:24 +0530

    Add files via upload

---
+++ src/App.js
@@ -0,0 +1,48 @@
+import React, { useState } from 'react';
+import './Manuscript.css';
+
+export default function App() {
+  const [text, setText] = useState(`In principio creavit Deus caelum et terram. terra autem erat inanis et vacua et tenebrae super faciem abyssi et spiritus Dei ferebatur super aquas. dixitque Deus fiat lux et facta est lux.`);
+
+
+  const getFormattedText = () => {
+    const firstLetter = text.charAt(0);
+    const rest = text.slice(1);
+    return (
+      <p className="manuscript-text">
+        <span className="illuminated-letter">{firstLetter}</span>
+        {rest}
+      </p>
+    );
+  };
+
+  const printPage = () => window.print();
+
+  return (
+    <div className="page-container">
+      <header className="header">
+        <h1>Welcome to the World of Illuminated Manuscripts</h1>
+      </header>
+
+      <main className="main-content">
+        <div className="manuscript-container">
+          <textarea
+            className="input-box"
+            value={text}
+            onChange={(e) => setText(e.target.value)}
+          />
+          <div className="output parchment" id="print-area">
+            {getFormattedText()}
+          </div>
+        </div>
+
+        <button onClick={printPage}>Print Manuscript</button>
+      </main>
+
+      <footer className="footer">
+        <p>&copy; {new Date().getFullYear()} Mahen Mahindaratne | All rights reserved</p>
+        <p>Designed by <a href="https://code.mahen-mahindaratne.online" target="_blank" rel="noopener noreferrer" class="underline hover:text-yellow-500">The Ghost Protocol</a></p>
+      </footer>
+    </div>
+  );
+}
